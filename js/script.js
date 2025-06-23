593487346dbfbbe13309003721c0c8a6242e2752
Nahuel de Soza <nahueldesoza@gmail.com>
2025-06-22 23:26:46 -0300

    agregado de objetos, modificaciones final para entrega 1

--- js/script.js
+++ js/script.js
@@ -27,3 +27,16 @@ for (i=0;i<arrayVacio.length;i++){
     alert ("El nombre es: "+arrayVacio[i])
 }
 alert ("Ingresando datos para entradas")
+let datosEntradas = []
+let nombreEntrada = prompt("Ingrese el nombre para la entrada del recital (valor cero para salir):")
+while (nombreEntrada != 0){
+    let apellidoEntrada = prompt("Ingrese el apellido para el entrada del recital:")
+    let fechaEntrada = prompt("Ingrese la fecha para el entrada del recital (1, 2 o 3, segun el dia):")
+    let objetoEntrada = {nombre: nombreEntrada, apellido: apellidoEntrada,fecha: fechaEntrada}
+    datosEntradas.push (objetoEntrada)
+    nombreEntrada = prompt("Ingrese los nombres para la entrada del recital (valor cero para salir):")
+}
+alert (datosEntradas.length)
+for (i=0;i<datosEntradas.length;i++){
+    alert ("El cliente "+ datosEntradas[i].nombre +" con apellido "+ datosEntradas[i].apellido +" saco entrada para la fecha "+ datosEntradas[i].fecha)
+}
